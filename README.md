57d4c7bc583d3d4a45c9ba06f099ac15d9ea465b
cedric <cedric___100@hotmail.co.uk>
2017-10-07 03:52:10 +0100

    Modified the readme.md file

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Bookmarks Store
+It's a very simple application storing data onto localStorage. It works by giving the site name and the site URL to save data on submit.
+
+#### Technologies
+1. HTML5
+1. CSS3
+    1. SASS
+1. JAVASCRIPT
+1. GULP/ GIT
---
+++ src/js/main.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// Get Form element
+var myForm = document.getElementById('myForm');
+
+function saveBookmark(e) {
+
+    // Get input values
+    var siteName = document.getElementById('siteName').value;
+    var siteURL = document.getElementById('siteURL').value;
+
+    // Storing values in a JSON object
+    var bookmarks = {
+        name : siteName,
+        url : siteURL
+    };
+
+    // Storing to localStorage
+    if (typeof(Storage) !== 'undefined') { // Check if browser support
+        localStorage.setItem('test', 'Hello World!');
+    } else {
+        console.log('Browser not compatible with Web Storage');
+    }
+
+    //console.log(bookmarks);
+    e.preventDefault();
+}
+
+ myForm.addEventListener('submit', saveBookmark);
