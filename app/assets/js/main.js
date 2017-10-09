d12ff77703a44dc39738942fb333f07d47e6bdfe
cedric <cedric___100@hotmail.co.uk>
2017-10-09 23:25:33 +0100

    Added display function when form is submited

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -10,20 +10,29 @@
 
 var key = 'bookmarks';
 var text = '';
+var i = 0;
 
-function checkWebStorage() {
+function validateForm(name, url) {
 
-      if (typeof(Storage) === null) {
-            text += 'Please upgrade your browser.';
-            window.alert(text);
+      var warning = document.querySelector('.warning');
+
+      if (name === '' || url === '') {
+            warning.style.display = 'block';
+            warning.innerHTML = 'Please fill the missing form.';
+            return false;
       } else {
-
-            if (window.localStorage.getItem(key)) {
-                  getBookmarks();
-            } else {
-                  console.log('empty');
-            }
+            
       }
+
+      return false;
+}
+
+function displayButton() {
+
+      var displayBtn = document.getElementById('btn-display');
+      displayBtn.style.display = 'block';
+      
+      displayBtn.addEventListener('click', getBookmarks);
 }
 
 // Save bookmarks to localStorage
@@ -32,6 +41,8 @@ function saveBookmarks(e) {
       // Get values 
       var siteName = document.getElementById('siteName').value;
       var siteURL = document.getElementById('siteURL').value;
+
+      //validateForm(siteName, siteURL);
 
       // Store values to JSON format
       var bookmark = {
@@ -48,6 +59,7 @@ function saveBookmarks(e) {
             bookmarks.push(bookmark);
             // Store array to the localStorage
             window.localStorage.setItem(key, JSON.stringify(bookmarks));// Convert to string
+            displayButton();
       } else {
             // Get bookmarks from localStorage
             var bookmarks = JSON.parse(window.localStorage.getItem(key));// Convert to JSON
@@ -61,22 +73,69 @@ function saveBookmarks(e) {
       e.preventDefault();
 }
 
+function deleteBookmarks(url) {
+      
+      // Get bookmarks from localStorage
+      var bookbmarks = JSON.parse(window.localStorage.getItem(key));
+
+      // Loop throught bookmarks URL
+      for (; i < bookbmarks.length; i++) {
+            var link = bookbmarks[i].url;
+            console.log(link);
+      }
+      
+      //console.log(url);
+}
+
 function getBookmarks() {
 
       // Get bookmarks from localStorage
       var bookmarks = JSON.parse(window.localStorage.getItem(key));
 
-      // Loop through # bookmarks
+      // Get the results UI
+      var resultsWrap = document.getElementById('results-wrap');
       
-      /*for (var i = 0; i < bookmarks.length; i++) {
-            var name = bookmarks[i].name;
-            var url = bookmarks[i].url;
-            console.log(name);
-            
-      }*/
+      //resultsWrap.innerHTML = '';
 
+      // Loop through # bookmarks
+      if (bookmarks !== null) {
+            for (; i < bookmarks.length; i++) {
+                  var name = bookmarks[i].name;
+                  var url = bookmarks[i].url;
+                  text = 'delete';
+                  // Build Output UI
+                  var card = '<div class="bookmarks-results">' + 
+                              // Left Column 
+                              '<div class="left-column">' + 
+                              '<h2>' + name + '</h2>' + 
+                              '<a href="'+ url +'" target="_blank">Visit</a>' + 
+                              '</div>' + 
+                              // Second Column 
+                              '<div class="right-column">' + 
+                              '<a href="#" class="btn btn-red delete-bookmarks" onclick="deleteBookmarks(\''+ url +'\')">' + text + '</a>' +
+                              '</div>' + 
+                              '</div>'; 
+                  resultsWrap.innerHTML += card;
+            }
+      }
+      
 }
-getBookmarks();
+
+// Check if browser support Web Storage (localStorage)
+function checkWebStorage() {
+      
+      if (typeof(Storage) === null) {
+            text += 'Please upgrade your browser.';
+            window.alert(text);
+      } else {
+            // Check if the key exist/empty
+            if (window.localStorage.getItem(key)) {
+                  displayButton();
+            } else {
+                  console.log('localStorage empty');
+            }
+      }
+}
 
 // Add submit event to the form ele
 document.getElementById('myForm').addEventListener('submit', saveBookmarks);
